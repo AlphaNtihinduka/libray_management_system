a15408e69e1829035b881bbe8d7d91b5d7163b32
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-09-25 10:48:54 +0300

    Setup

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello");
     }
 }
+
+//Main Menu:
+//        1.	Login as Student
+//o	Prompt for a membership ID and password.
+//o	If the credentials are correct, proceed to the Member Menu.
+//o	If the credentials are incorrect, return to the Main Menu.
+//        2.	Login as Librarian
+//o	Prompt for a librarian ID and password.
+//o	If the credentials are correct, proceed to the Librarian Menu.
+//o	If the credentials are incorrect, return to the Main Menu.
+//        3.	Login as Admin
+//o	Prompt for an admin ID and password.
+//o	If the credentials are correct, proceed to the Admin Menu.
+//o	If the credentials are incorrect, return to the Main Menu.
+//        4.	Register
+//o	Prompt for a name, user ID, password, and user type (Member, Librarian, or Admin).
+//o	Register the new user with the provided details.
+//o	Return to the Main Menu.
+//        5.	Exit
+//o	Close the application.
