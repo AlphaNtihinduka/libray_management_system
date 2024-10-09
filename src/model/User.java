148eb46c3409f6b2085e207178295e5796634ae5
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-10-09 11:01:58 +0300

    Create getters and setters

--- src/model/User.java
+++ src/model/User.java
@@ -12,6 +12,43 @@ public abstract class User {
         this.userName = userName;
         this.password = password;
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return ;
+    }
 }
 
 
